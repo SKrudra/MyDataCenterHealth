5f57e44a9bef00461edb83e978a4f2d9fc36643a
Sandeep Kumar <sandeep.kumar@transplace.com>
2021-12-29 07:33:36 -0600

    ViewChild and "!:" non-null assertion operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'MyDataCenterHealth';
+  @ViewChild(DashboardComponent) dashboard!: DashboardComponent; // Non-null assertion operator
+
+  refresh() {
+    this.dashboard.generateData();
+  }
 }
